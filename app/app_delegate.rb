0c77f0f7090a866402580c2587d70a6ab9abe526
Paul Campbell <paul@rslw.com>
2014-09-22 21:30:10 +0100

    add the hello world alert

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    alert = UIAlertView.new
+    alert.message = "Hello World!"
+    alert.show
     true
   end
 end
