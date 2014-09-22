92da739fa34299231693e5abce8601801550d17e
Paul Campbell <paul@rslw.com>
2014-09-22 22:26:11 +0100

    Set the width and height of the image, set the main view bg to white

--- app/layouts/home_screen_layout.rb
+++ app/layouts/home_screen_layout.rb
@@ -1,13 +1,19 @@
 class HomeScreenLayout < MotionKit::Layout
   def layout
-    add UIImageView, :logo
+    root :home_screen_view do
+      add UIImageView, :logo
+    end
+  end
+
+  def home_screen_view_style
+    background_color UIColor.whiteColor
   end
 
   def logo_style
     top 0
     left 0
-    width '100%'
-    height '100%'
+    width '289'
+    height '234'
     image UIImage.imageNamed('belfast-ruby')
   end
 end
